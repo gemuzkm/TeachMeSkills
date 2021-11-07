2c9b5d1484e00b5f6341f9174c1c87c0d62f0ee6
gemuz_home <gemuzkm@gmail.com>
2021-11-07 23:10:23 +0300

    package com.lesson6dz beta + sout in main

--- src/com/lesson6dz/TransportMain.java
+++ src/com/lesson6dz/TransportMain.java
@@ -6,21 +6,25 @@ public class TransportMain {
         passengerBmw.Description();
         passengerBmw.FuelTimeMaxSpeed(2.5);
 
+        System.out.println();
         Truck truckVolvo = new Truck(600, 100, 12000, "Volvo", 12, 25, 15);
         truckVolvo.Description();
         truckVolvo.LoadGood(3);
         truckVolvo.LoadGood(20);
 
+        System.out.println();
         Civil civilAirBus = new Civil(1500, 950, 40000, "AirBus", 40, 1500, 320, true);
         civilAirBus.Description();
         civilAirBus.PassengerGood(160);
         civilAirBus.PassengerGood(400);
 
+        System.out.println();
         Military militarySy27 = new Military(400, 1200, 5000, "СУ-27", 10, 250, true, 18);
         militarySy27.Description();
         militarySy27.Shot();
         militarySy27.Ejection();
 
+        System.out.println();
         Military militarySy271 = new Military(400, 1200, 5000, "СУ-271", 10, 250, false, 0);
         militarySy271.Description();
         militarySy271.Shot();
