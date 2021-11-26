80fbf65dd7fff795f1eb8729adc2e2547f1b8478
gemuz_home <gemuzkm@gmail.com>
2021-11-26 21:44:43 +0300

    com.lesson12 компоратор

--- src/com/lesson12/Main3.java
+++ src/com/lesson12/Main3.java
@@ -2,6 +2,7 @@ package com.lesson12;
 
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 
 public class Main3 {
     public static void main(String[] args) {
@@ -15,7 +16,17 @@ public class Main3 {
         System.out.println(set.contains(1));
         System.out.println(set);
 
+        System.out.println("MAP");
         HashMap<String, Integer> map = new HashMap<>();
+        map.put("1", 100);
+        map.put("2", 200);
+        map.put("3", 300);
+        System.out.println(map.get("2"));
+        System.out.println(map.containsKey("2"));
+        System.out.println(map);
+        for (Map.Entry<String, Integer> i : map.entrySet()) {
+            System.out.println(i);
+        }
 
     }
 }
---
+++ src/com/lesson12/Map4.java
@@ -0,0 +1,9 @@
+package com.lesson12;
+
+//компоратор
+
+public class Map4 {
+    public static void main(String[] args) {
+
+    }
+}
