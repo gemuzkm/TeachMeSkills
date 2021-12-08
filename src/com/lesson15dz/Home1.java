a4dc32244fa56c7e34420ca941752e9589d306e5
gemuz_home <gemuzkm@gmail.com>
2021-12-08 21:22:35 +0300

    com.lesson15dz Home2

--- src/com/lesson15dz/Home1.java
+++ src/com/lesson15dz/Home1.java
@@ -14,7 +14,7 @@ import java.util.regex.Pattern;
 public class Home1 {
     public static void main(String[] args) {
         List<String> listTel = new ArrayList<>();
-        listTel.add("+375(29)123-12-12");
+        listTel.add(" +375(29)123-12-12 ");
         listTel.add("375(29)123-12-12");
         listTel.add("+375(29)123-1212");
         listTel.add("+375(29)12312-12");
@@ -22,16 +22,16 @@ public class Home1 {
         listTel.add("+375(29123-12-12");
         listTel.add("+37529)123-12-12");
         listTel.add("+37529123-12-12");
-        listTel.add("+375(25)123-80-11");
-        listTel.add("+375(44)123-80-11");
-        listTel.add("+375(33)123-80-11");
+        listTel.add("+375(25)123-80-11  ");
+        listTel.add("  +375(44)123-80-11");
+        listTel.add("+375(33)123-80-11  ");
 
         Pattern patternTel = Pattern.compile("^\\+375\\((25|29|33|44)\\)\\d{3}-\\d{2}-\\d{2}");
 
         for (String itemTel: listTel) {
-            Matcher matcher = patternTel.matcher(itemTel);
+            Matcher matcher = patternTel.matcher(itemTel.trim());
             if (matcher.find()) {
-                System.out.println(itemTel);
+                System.out.println(itemTel.trim());
             }
         }
     }
---
+++ src/com/lesson15dz/Home2.java
@@ -0,0 +1,35 @@
+package com.lesson15dz;
+/*
+2) Написать регулярное выражение для проверки правильности ввода email. Email считается валидным, если у него есть '@что-то.что-то'
+*/
+
+import javax.swing.text.html.parser.Parser;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Home2 {
+    public static void main(String[] args) {
+        List<String> listMail = new ArrayList<>();
+
+        listMail.add(" s das ad a@gmail.ru");
+        listMail.add("dasdasd@ mail.ru");
+        listMail.add("asdasdasd@gmail.com");
+        listMail.add("griinfewi@gmailc .com");
+        listMail.add(" s123dds@gmail.com  ");
+        listMail.add(" s123dds@123123.com  ");
+        listMail.add(" 111111@123123.com  ");
+        listMail.add(" s123dds@gmail.123  ");
+        listMail.add(" 123123@123123.123  ");
+
+        Pattern parrentMail = Pattern.compile("^[a-zA-Z0-9]+@+[a-zA-Z0-9]+\\.+[a-zA-Z]");
+
+        for (String itemMail: listMail) {
+            Matcher matcherMail = parrentMail.matcher(itemMail.trim());
+            if (matcherMail.find()) {
+                System.out.println(itemMail.trim());
+            }
+        }
+    }
+}
