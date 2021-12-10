790c4b276cb69c7e7689ec68083d600bdf9118a3
gemuz_home <gemuzkm@gmail.com>
2021-12-10 23:45:41 +0300

    com.lesson16dz add Storage class

---
+++ src/com/lesson16dz/Storage.java
@@ -0,0 +1,19 @@
+package com.lesson16dz;
+
+public class Storage {
+    private String fileNameUniversity = "src/com/lesson16dz/university.txt";
+    private String fileNameGroups = "src/com/lesson16dz/group.txt";
+    private String fileNameStudents = "src/com/lesson16dz/student.txt";
+
+    public void add(Student student) {
+
+    }
+
+    public void add(Group group) {
+
+    }
+
+    public void add(University university) {
+
+    }
+}
--- src/com/lesson16dz/Student.java
+++ src/com/lesson16dz/Student.java
@@ -3,6 +3,7 @@ package com.lesson16dz;
 import java.util.List;
 
 public class Student {
+    private int id;
     private String firsName;
     private String lastName;
     private List<Integer> listRatings;
@@ -10,12 +11,52 @@ public class Student {
     public Student() {
     }
 
-    public Student(String firsName, String lastName, List<Integer> listRatings) {
+    public Student(int id, String firsName, String lastName, List<Integer> listRatings) {
+        this.id = id;
         this.firsName = firsName;
         this.lastName = lastName;
         this.listRatings = listRatings;
-
     }
 
+    public int getId() {
+        return id;
+    }
 
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirsName() {
+        return firsName;
+    }
+
+    public void setFirsName(String firsName) {
+        this.firsName = firsName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public List<Integer> getListRatings() {
+        return listRatings;
+    }
+
+    public void setListRatings(List<Integer> listRatings) {
+        this.listRatings = listRatings;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", firsName='" + firsName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", listRatings=" + listRatings +
+                '}';
+    }
 }
