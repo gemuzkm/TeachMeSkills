0456430bcd34713575d316aa7aaf98d7e207a180
gemuz_work <gemuzkm@gmail.com>
2021-12-11 09:37:28 +0300

    com.lesson16dz txt - > json

--- src/com/lesson16dz/Storage.java
+++ src/com/lesson16dz/Storage.java
@@ -1,9 +1,9 @@
 package com.lesson16dz;
 
 public class Storage {
-    private String fileNameUniversity = "src/com/lesson16dz/university.txt";
-    private String fileNameGroups = "src/com/lesson16dz/group.txt";
-    private String fileNameStudents = "src/com/lesson16dz/student.txt";
+    private String fileNameUniversity = "src/com/lesson16dz/university.json";
+    private String fileNameGroups = "src/com/lesson16dz/group.json";
+    private String fileNameStudents = "src/com/lesson16dz/student.json";
 
     public void add(Student student) {
 
