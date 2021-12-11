bee1a9ba23e80b1050fd3231e50c8add27895b43
gemuz_work <gemuzkm@gmail.com>
2021-12-11 13:11:52 +0300

    com.lesson16dz add 4 test university

--- src/com/lesson16dz/Storage.java
+++ src/com/lesson16dz/Storage.java
@@ -9,8 +9,8 @@ public class Storage {
     private String fileNameStudents = "src/com/lesson16dz/student.json";
 
     public void add(Student student) {
-        if (Student.listIdStudents.contains(student.getId()))  {
-            System.out.println("Студет с таким ID = "+ student.getId() + " уже существует\n");
+        if (Student.listIdStudents.contains(student.getId())) {
+            System.out.println("Студет с таким ID = " + student.getId() + " уже существует\n");
         } else {
             Student.listIdStudents.add(student.getId());
             ObjectMapper objectMapper = new ObjectMapper();
@@ -24,13 +24,15 @@ public class Storage {
         }
     }
 
-    public Student getStudent (int idStudent) {
+    public Student getStudent(int idStudent) {
         return new Student();
     }
 
     public void add(Group group) {
-        if (Group.listIdUGroup.contains(group.getId()))  {
-            System.out.println("Группа с таким ID = "+ group.getId() + " уже существует\n");
+        if (Student.listIdStudents.size() == 0) {
+            System.out.println("Группа не добавлена. Нужно добавить студентов\n");
+        } else if (Group.listIdUGroup.contains(group.getId())) {
+            System.out.println("Группа с таким ID = " + group.getId() + " уже существует\n");
         } else {
             Group.listIdUGroup.add(group.getId());
             ObjectMapper objectMapper = new ObjectMapper();
@@ -49,7 +51,21 @@ public class Storage {
     }
 
     public void add(University university) {
-
+        if (Group.listIdUGroup.size() == 0) {
+            System.out.println("Университет не добавлен. Нужно добавить группы\n");
+        } else if (University.listIdUniversity.contains(university.getId())) {
+            System.out.println("Университет с таким ID = " + university.getId() + " уже существует\n");
+        } else {
+            University.listIdUniversity.add(university.getId());
+            ObjectMapper objectMapper = new ObjectMapper();
+            try {
+                String json = objectMapper.writeValueAsString(university);
+                System.out.println(json);
+            } catch (JsonProcessingException e) {
+                e.printStackTrace();
+            }
+            System.out.println("Университет с ID = " + university.getId() + " успешно добавлен!\n");
+        }
     }
 
     public University getUniversity(int idUniversity) {
--- src/com/lesson16dz/University.java
+++ src/com/lesson16dz/University.java
@@ -1,20 +1,32 @@
 package com.lesson16dz;
 
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
 public class University {
+    private int id;
     private String name;
-    private List<Group> listGroup;
+    private Set<Integer> listIdGroupInUniversity = new HashSet<>();
+    private Storage storage = new Storage();
     public static Set<Integer> listIdUniversity = new HashSet<>();
 
     public University() {
     }
 
-    public University(String name, List<Group> listGroup) {
+    public University(int id, String name, Set<Integer> listIdGroupInUniversity) {
+        this.id = id;
         this.name = name;
-        this.listGroup = listGroup;
+        this.listIdGroupInUniversity = listIdGroupInUniversity;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
     }
 
     public String getName() {
@@ -25,25 +37,45 @@ public class University {
         this.name = name;
     }
 
-    public List<Group> getListGroup() {
-        return listGroup;
+    public Set<Integer> getListIdGroupInUniversity() {
+        return listIdGroupInUniversity;
     }
 
-    public void setListGroup(List<Group> listGroup) {
-        this.listGroup = listGroup;
-    }
-
-    @Override
-    public String toString() {
-        return "University{" +
-                "name='" + name + '\'' +
-                ", listGroup=" + listGroup +
-                '}';
+    public void setListIdGroupInUniversity(Set<Integer> listIdGroupInUniversity) {
+        this.listIdGroupInUniversity = listIdGroupInUniversity;
     }
 
     public void addUniversity() {
         System.out.println("\nДобавление университетов....\n");
 
+        University university1 = new University(0, "university1", new HashSet<>(List.of(0,1)));
+        storage.add(university1);
 
+        try {
+            Thread.sleep(500);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        University university2 = new University(1, "university2", new HashSet<>(List.of(2,3)));
+        storage.add(university2);
+
+        try {
+            Thread.sleep(500);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        University university3 = new University(2, "university3", new HashSet<>(List.of(4)));
+        storage.add(university3);
+
+        try {
+            Thread.sleep(500);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        University university4 = new University(2, "university4", new HashSet<>(List.of(4)));
+        storage.add(university4);
     }
 }
