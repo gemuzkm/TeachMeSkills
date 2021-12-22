c1f424cfd0373ea3cc2ed9ac84493dbab72019a0
gemuz_work <gemuzkm@gmail.com>
2021-12-22 13:54:40 +0300

    com.lesson18dz init DB

--- src/com/lesson18/Main.java
+++ src/com/lesson18/Main.java
@@ -12,13 +12,16 @@ public class Main {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver").getDeclaredConstructor().newInstance();
             try (Connection connection = DriverManager.getConnection(url, username, password)) {
-//                Statement statement = connection.createStatement();
+                Statement statement = connection.createStatement();
 
 //                statement.execute("create table users(" +
 //                        "id int auto_increment primary key," +
 //                        "name varchar(30) not null," +
 //                        "age int not null" +
-//                        ")");]
+//                        ")");
+
+
+
 //                statement.execute("insert into users(name, age) " +
 //                        "values ('vasya', 50)");
 //                statement.execute("insert into users(name, age) " +
@@ -37,16 +40,16 @@ public class Main {
 //                Statement statement = connection.createStatement();
 //                ResultSet resultSet = statement.executeQuery("select * from users");
 
-                Statement deleteStatement = connection.createStatement();
-                deleteStatement.execute("delete from  users where id = 2");
-
-                Statement statement = connection.createStatement();
-                ResultSet resultSet = statement.executeQuery("select * from users");
-                while (resultSet.next()) {
-                    System.out.println("id - " + resultSet.getInt(1) +
-                            ", name - " + resultSet.getString(2) +
-                            ", age - " + resultSet.getInt(3));
-                }
+//                Statement deleteStatement = connection.createStatement();
+//                deleteStatement.execute("delete from  users where id = 2");
+//
+//                Statement statement = connection.createStatement();
+//                ResultSet resultSet = statement.executeQuery("select * from users");
+//                while (resultSet.next()) {
+//                    System.out.println("id - " + resultSet.getInt(1) +
+//                            ", name - " + resultSet.getString(2) +
+//                            ", age - " + resultSet.getInt(3));
+//                }
             }
         } catch (InstantiationException | IllegalAccessException | InvocationTargetException | NoSuchMethodException | ClassNotFoundException | SQLException e) {
             e.printStackTrace();
---
+++ src/com/lesson18dz/Main.java
@@ -0,0 +1,9 @@
+package com.lesson18dz;
+
+public class Main {
+    public static void main(String[] args) {
+        String url = "jdbc:mysql://localhost:3306/nothwind";
+        String name = "root";
+        String password =  "admin";
+    }
+}
