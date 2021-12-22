96ade39ad9d776fa822bf768fb0bb03e15f44113
gemuz_work <gemuzkm@gmail.com>
2021-12-22 15:30:53 +0300

    com.lesson18dz insert into Customers

--- src/com/lesson18dz/Main.java
+++ src/com/lesson18dz/Main.java
@@ -27,6 +27,20 @@ public class Main {
 //                        "Country varchar(30) not null" +
 //                        ")");
 
+//                statement.execute("insert into Customers(CustomerName, ContactName, Address, City, PostalCode, Country) " +
+//                        "values('Alfreds Futterkiste', 'Maria Anders', 'Obere Str. 57', 'Berlin', '12209', 'Germany')");
+//
+//                statement.execute("insert into Customers(CustomerName, ContactName, Address, City, PostalCode, Country) " +
+//                        "values('Ana Trujillo Emparedados y helados', 'Ana Trujillo', 'Avda. de la Constitución 2222', 'México D.F.', '05021', 'Mexico')");
+//
+//                statement.execute("insert into Customers(CustomerName, ContactName, Address, City, PostalCode, Country) " +
+//                        "values('Antonio Moreno Taquería', 'Antonio Moreno', 'Mataderos 2312', 'México D.F.', '05023', 'Mexico')");
+//
+//                statement.execute("insert into Customers(CustomerName, ContactName, Address, City, PostalCode, Country) " +
+//                        "values('Around the Horn', 'Thomas Hardy', '120 Hanover Sq.', 'London', 'WA1 1DP', 'UK')");
+//
+//                statement.execute("insert into Customers(CustomerName, ContactName, Address, City, PostalCode, Country) " +
+//                        "values('Berglunds snabbköp', 'Christina Berglund', 'Berguvsvägen 8', 'Luleå', 'S-958 22', 'Sweden')");
 
             }
         } catch (InstantiationException | IllegalAccessException | InvocationTargetException | NoSuchMethodException | ClassNotFoundException | SQLException e) {
